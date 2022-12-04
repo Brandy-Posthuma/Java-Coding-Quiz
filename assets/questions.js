b60121027bb45b8a3869b94581aee8599a41c29d
Brandy Posthuma <blposthuma@yahoo.com>
2022-12-03 19:40:34 -0700

    worked on adding questions to JavaScript

---
+++ assets/questions.js
@@ -0,0 +1,58 @@
+// setting up an array and sending the questions, number, options, and answers
+let questions = [
+    {
+        number: 1,
+        question: "What is JavaScript?"
+        answer: JavaScript is a scripting language used along with HTML and CSS to make the website interactive 
+        options: [
+            "JavaScript is a scripting language used to make the website interactive",
+            "JavaScript is an assembly language used to make the website interactive",
+            "JavaScript is a compiled language used to make the website interactive",
+            "None of the above",
+        ]
+    },
+    {
+        number: 2,
+        question: "Which of the following statements is true about JavaScript?"
+        answer: JavaScript is an Object-Based language
+        options: [
+            "JavaScript is Assembly-language",
+            "JavaScript is a cup of coffee",
+            "JavaScript is an Object-Based language",
+            "JavaScript is a High-level language",
+        ]
+    },
+    {
+        number: 3,
+        question: Which of the following is not a JavaScript data type?
+        answer: All of the above
+        options: [
+            "Undefined",
+            "Number",
+            "Null",
+            "All of the Above",
+        ]
+    },
+    {
+        number: 4,
+        question: Which of the following can be used to call a JavaScript Code Snippet?
+        answer: Function/Method
+        options: [
+            "Function/Method",
+            "RMI",
+            "Preprocessor",
+            "Triggering Event",
+        ]
+    },
+    {
+        number: 5,
+        question: Which one of the following is known as a Conditional Expression?
+        answer: Immediate if
+        options: [
+            "If-then-else statement",
+            "Switch Statement",
+            "Immediate if",
+            "Alternative to if-else",
+        ]
+    }, 
+]
